1cf443009679e90a396cb76e022298aa9b851af9
Jacob Quatier <jquatier@gmail.com>
2016-09-01 21:57:29 -0700

    adding eslint config and fixing a few lint errors

--- browser.js
+++ browser.js
@@ -1,23 +1,22 @@
+/* eslint-env browser */
 const {Tracer, BatchRecorder, ExplicitContext} = require('zipkin');
 const {HttpLogger} = require('zipkin-transport-http');
+const wrapFetch = require('zipkin-instrumentation-fetch');
 
 // Send spans to the origin server under the path /zipkin
 const recorder = new BatchRecorder({
   logger: new HttpLogger({
-    endpoint: location.origin + '/zipkin'
+    endpoint: `${location.origin}/zipkin`
   })
 });
 
 const ctxImpl = new ExplicitContext();
 const tracer = new Tracer({ctxImpl, recorder});
-
-const wrapFetch = require('zipkin-instrumentation-fetch');
 const zipkinFetch = wrapFetch(fetch, {tracer, serviceName: 'browser'});
 
 // wrap fetch call so that it is traced
-var result = zipkinFetch('/api')
-result.then(function(response) {
-  return response.text()
-}).catch(function(ex) {
-  console.log('failed', ex)
-})
+zipkinFetch('/api')
+  .then((response) => (response.text()))
+  .catch((ex) => {
+    console.log('failed', ex);
+  });
--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const {Tracer, BatchRecorder, ExplicitContext} = require('zipkin');
 const {HttpLogger} = require('zipkin-transport-http');
-const zipkinBaseUrl = 'http://localhost:9411'
+
+const zipkinBaseUrl = 'http://localhost:9411';
 const recorder = new BatchRecorder({
   logger: new HttpLogger({
-    endpoint: zipkinBaseUrl + '/api/v1/spans'
+    endpoint: `${zipkinBaseUrl}/api/v1/spans`
   })
 });
 const ctxImpl = new ExplicitContext();
@@ -23,23 +24,21 @@ app.use(zipkinMiddleware({
 }));
 
 // Serve the bundled browser.js
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
   res.send('<html><body><script src="/bundle.js"></script></body></html>');
 });
 
 // Mount zipkin's POST endpoint to /zipkin for browsers to use
 // In real life, you can re-use your existing auth api when proxying
 app.use('/zipkin', proxy(zipkinBaseUrl, {
-  forwardPath: function(req, res) {
-    return '/api/v1/spans'
-  }
+  forwardPath: () => ('/api/v1/spans')
 }));
 
 // Actual http call that will end up in the trace
-app.get('/api', function (req, res) {
+app.get('/api', (req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(3000, function () {
+app.listen(3000, () => {
   console.log('Example app listening on port 3000!');
 });
