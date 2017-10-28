c8b1352e462d4c0beeb00f2f6334597d0dcc215e
Adrian Cole <acole@pivotal.io>
2017-10-28 21:03:40 +0800

    Updates to use zipkin2

--- recorder.js
+++ recorder.js
@@ -1,12 +1,16 @@
 /* eslint-env browser */
-const {BatchRecorder} = require('zipkin');
+const {
+  BatchRecorder,
+  jsonEncoder: {JSON_V2}
+} = require('zipkin');
 const {HttpLogger} = require('zipkin-transport-http');
 
 // Send spans to Zipkin asynchronously over HTTP
 const zipkinBaseUrl = 'http://localhost:9411';
 const recorder = new BatchRecorder({
   logger: new HttpLogger({
-    endpoint: `${zipkinBaseUrl}/api/v1/spans`
+    endpoint: `${zipkinBaseUrl}/api/v2/spans`,
+    jsonEncoder: JSON_V2
   })
 });
 
