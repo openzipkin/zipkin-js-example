704724c77be7999ff5f3d7ef2311d36cb4379fad
asanluis <a.sanluismoure@gmail.com>
2020-04-17 03:05:30 +0200

    add aws localstack test

--- README.md
+++ README.md
@@ -6,3 +6,5 @@ You can find example usages for zipkin.js in different scenarios:
 
 - [web](web/)
 - [react-native](react-native-example/)
+
+Add AWS Stack test, you have a docker-compose to get start wit zipkin-aws(SQS Collector, Kinesis Collector and AWS ES) and localstack(mock AWS Services like SQS)
--- web/recorder.js
+++ web/recorder.js
@@ -1,46 +1,24 @@
 /* eslint-env browser */
 const {
-  BatchRecorder,
-  jsonEncoder: {JSON_V2}
+    BatchRecorder
 } = require('zipkin');
-const {HttpLogger} = require('zipkin-transport-http');
+const {AwsSqsLogger} = require('zipkin-transport-aws-sqs');
 
-const debug = 'undefined' !== typeof window
-  ? window.location.search.indexOf('debug') !== -1
-  : process.env.DEBUG;
-
-// Send spans to Zipkin asynchronously over HTTP
-const zipkinBaseUrl = 'http://localhost:9411';
-
-const httpLogger = new HttpLogger({
-  endpoint: `${zipkinBaseUrl}/api/v2/spans`,
-  jsonEncoder: JSON_V2
+let awsSqsLogger = new AwsSqsLogger({
+    queueUrl: "http://localhost:4576/queue/sqs-test", //mandatory
+    region: 'us-east-1', // optional, region string
+    credentialProvider: {
+        accessKeyId: 'test',
+        secretAccessKey: 'test'
+    },
+    delaySeconds: 0// optional
 });
 
+
 function recorder(serviceName) {
-  return debug ? debugRecorder(serviceName) : new BatchRecorder({logger: httpLogger});
-}
-
-function debugRecorder(serviceName) {
-  // This is a hack that lets you see the data sent to Zipkin!
-  const logger = {
-    logSpan: (span) => {
-      const json = JSON_V2.encode(span);
-      console.log(`${serviceName} reporting: ${json}`);
-      httpLogger.logSpan(span);
-    }
-  };
-
-  const batchRecorder = new BatchRecorder({logger});
-
-  // This is a hack that lets you see which annotations become which spans
-  return ({
-    record: (rec) => {
-      const {spanId, traceId} = rec.traceId;
-      console.log(`${serviceName} recording: ${traceId}/${spanId} ${rec.annotation.toString()}`);
-      batchRecorder.record(rec);
-    }
-  });
+    return new BatchRecorder({
+        logger: awsSqsLogger
+    });
 }
 
 module.exports.recorder = recorder;
